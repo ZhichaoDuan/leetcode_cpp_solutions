1ea19277fbb9a677efe9a3fa2963686671075df8
ZhichaoDuan <zcduan55@gmail.com>
2019-03-28 09:32:09 +0000

    Create 22.cpp

---
+++ 22.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        std::vector<std::string> result;
+        generate("", n, n, result);
+        return result;
+    }
+private:
+    void generate(std::string item, int left, int right, std::vector<std::string> &result) {
+        if (left==0&&right==0) {
+            result.push_back(item);
+            return;
+        }
+        if (left>0) {
+            generate(item + '(' , left-1, right, result);
+        }
+        if (left < right) {
+            generate(item+')', left, right-1, result);
+        }
+    }
+};
