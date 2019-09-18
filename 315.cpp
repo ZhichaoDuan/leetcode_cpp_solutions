c9cf71e741b06cd0c28a0d29fa496731c82d6b50
matthew <zcduan55@gmail.com>
2019-09-18 17:07:08 +0800

    updated 315

--- 315.cpp
+++ 315.cpp
@@ -1,62 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+class TN {
+public:
+    int val;
+    int count;
+    TN *left;
+    TN *right;
+    TN(int v) : val(v), left(NULL), right(NULL), count(1) {}
+};
+
 class Solution {
 public:
+    TN *root;
     vector<int> countSmaller(vector<int>& nums) {
-        std::vector<std::pair<int, int>> vec;
-        std::vector<int> count;
-        for (int i=0; i<nums.size(); i++) {
-            vec.push_back(std::make_pair(nums[i], i));
-            count.push_back(0);
+        vector<int> res;
+        if (nums.size()==0) return res;
+        
+        root = new TN(nums[nums.size()-1]);
+        res.push_back(0);
+        for (int i=nums.size()-2;i>=0;i--) {
+            int c = insert_node(*root, nums[i]);
+            res.push_back(c);
         }
-        merge_sort(vec, count);
-        return count;
+        reverse(res.begin(), res.end());
+        return res;
+    }   
+
+    int insert_node(TN &root, int val) {
+        TN *node = &root;
+        int t_count = 0;
+        while(true) {
+            if (val <= node->val) {
+                node->count++;
+                // cout<<node->left->val<<"asd"<<endl;
+                if (node->left==NULL) {
+                    node->left = new TN(val);
+                    break;
+                } else {
+                    node = node->left;
+                }
+            } else {
+                t_count += node->count;
+                if (node->right==NULL) {              
+                    node->right = new TN(val);                        
+                    break;
+                } else {                    
+                    node = node->right;
+                }
+            }
+            
+        }
+
+        return t_count;
     }
     
-private:
-    void merge_sort(std::vector<std::pair<int, int>> &vec,
-                   std::vector<int> &count) {
-        if (vec.size()<2) {
-            return ;
-        }
-        int mid = vec.size()/2;
-        std::vector<std::pair<int, int>> sub_vec1;
-        std::vector<std::pair<int, int>> sub_vec2;
-        for (int i=0; i<mid;i++) {
-            sub_vec1.push_back(vec[i]);
-        }
-        for (int i=mid;i<vec.size();i++){
-            sub_vec2.push_back(vec[i]);
-        }
-        merge_sort(sub_vec1, count);
-        merge_sort(sub_vec2, count);
-        vec.clear();
-        merge_sort_two_vec(sub_vec1, sub_vec2, vec, count);
-    }
-    
-    void merge_sort_two_vec(std::vector<std::pair<int, int>> &sub_vec1,
-                            std::vector<std::pair<int, int>> &sub_vec2,
-                            std::vector<std::pair<int, int>> &vec,
-                            std::vector<int> &count) {
-        int i = 0;
-        int j = 0;
-        while(i<sub_vec1.size()&&j<sub_vec2.size()) {
-            if(sub_vec1[i].first<= sub_vec2[j].first) {
-                count[sub_vec1[i].second] += j;
-                vec.push_back(sub_vec1[i]);
-                i++;
-            }
-            else {
-                vec.push_back(sub_vec2[j]);
-                j++;
-            }
-        }
-        
-        for(; i<sub_vec1.size(); i++) {
-            count[sub_vec1[i].second] += j;
-            vec.push_back(sub_vec1[i]);
-        }
-        
-        for(;j<sub_vec2.size();j++) {
-            vec.push_back(sub_vec2[j]);
-        }
-    }
 };
+
+
+
+int main() {
+    vector<int> v({5, 2, 6, 1});
+    Solution sol;
+    vector<int> res =  sol.countSmaller(v);
+    cout<<"haha"<<res[0]<<res[1]<<res[2]<<res[3]<<endl;
+    return 0;
+}
+
